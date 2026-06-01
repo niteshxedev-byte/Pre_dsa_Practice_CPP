c0186cdfe978e690b20c25cdf42f6b081c80b50e
Nitesh Kumar Vashisht <niteshxe@gmail.com>
2026-06-01 21:35:32 +0530

    5 pattern qustions done

--- pre_dsa_questions/pattern_programming_cpp/question_1_pattern_programming.cpp
+++ pre_dsa_questions/pattern_programming_cpp/question_1_pattern_programming.cpp
@@ -1,21 +1,61 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-
-
-void square(int x){
-       for(int i =0;i<x;i++){
-     for(int j=0;j<x;j++){
-         cout<<"*";
-     }
-     cout<<endl;
-     
+void print_pattern_1(int c){
+    for(int i=0;i<c;i++){
+        for(int j=0;j<c;j++){
+            cout<<"*";
+        }
+        cout<<endl;
     }
-   
 }
 
+void print_pattern_2(int c){
+    for(int i =0 ;i<c;i++){
+        for(int j =0 ; j<i+1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+void print_pattern_3(int c){
+    cout<<c<<endl;
+    for(int i=0 ;i<c;i++){
+        for(int j = 5;j>i;j--){
+          cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+
+
+void print_pattern_4(int c){
+    for(int i=0;i<c;i++){
+        for(int j=1;j<=i+1;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+
+
+void print_pattern_5(int c){
+  for(int i =0 ;i<c;i++){
+      for(int j=0;j<i+1;j++){
+          cout<<i+1;
+      }
+      cout<<endl;
+  }
+}
+
+
 int main(){
- print_pattern_1(4);
- 
-   return 0 ;
+    print_pattern_1(5);
+    print_pattern_2(5);
+    print_pattern_3(5);
+    print_pattern_4(5);
+       print_pattern_5(5);
+    return 0;
 }
